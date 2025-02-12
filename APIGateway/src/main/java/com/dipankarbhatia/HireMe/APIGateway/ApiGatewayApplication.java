7fe491edd7402e65b8316fd9adcc6b0dedb9ea27
Dipankar Bhatia <bhatia.dipankar@gmail.com>
2025-02-12 17:02:01 -0500

    Ignore hireme-clean directory

--- APIGateway/src/main/java/com/dipankarbhatia/HireMe/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/dipankarbhatia/HireMe/APIGateway/ApiGatewayApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Acti
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
